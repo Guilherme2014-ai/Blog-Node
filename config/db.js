2da972dec529d5629d5b043dfe282e97d2701cdb
guilherme <guilhermehenrique78383@gmail.com>
2021-01-10 20:43:44 -0300

    Correcao 1

--- config/db.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://chat:guilherme2014@chat.gtuwm.mongodb.net/chat?retryWrites=true&w=majority'}
-}else {
-    module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
-}
